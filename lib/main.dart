4d3e98c78f91779b66a3b061a7c9940bb3f9746e
Ral Oliver <raloliver.ws@gmail.com>
2019-08-07 21:24:28 -0300

    Init with forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
-
-import 'models/todo.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,117 +6,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'WYN',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: HomePage(),
+      home: new Form(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  var items = new List<Item>();
-
-  HomePage() {
-    items = [];
-    // todo.items.add(Item(title: "Item 1", done: false));
-    // todo.items.add(Item(title: "Item 2", done: true));
-    // todo.items.add(Item(title: "Item 3", done: false));
-  }
-
+class Form extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  State<StatefulWidget> createState() => _FormState();
 }
 
-class _HomePageState extends State<HomePage> {
-  TextEditingController newTaskCtrl = TextEditingController();
-
-  void add() {
-    setState(() {
-      widget.items.add(Item(title: newTaskCtrl.text, done: false));
-      newTaskCtrl.text = "";
-      save();
-    });
-  }
-
-  void remove(index) {
-    setState(() {
-      widget.items.removeAt(index);
-      save();
-    });
-  }
-
-  save() async {
-    var prefs = await SharedPreferences.getInstance();
-    await prefs.setString('data', jsonEncode(widget.items));
-  }
-
-  Future loadData() async {
-    var prefs = await SharedPreferences.getInstance();
-    var data = prefs.getString('data');
-    if (data != null) {
-      Iterable decoded = jsonDecode(data);
-      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
-      setState(() {
-        widget.items = result;
-      });
-    }
-  }
-
-  _HomePageState() {
-    loadData();
-  }
+class _FormState extends State<Form> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextFormField(
-          keyboardType: TextInputType.text,
-          controller: newTaskCtrl,
-          decoration: InputDecoration(
-            labelText: "Nova Tarefa",
-            labelStyle: TextStyle(color: Colors.white),
-          ),
-          validator: (value) {
-            if (value.isEmpty) {
-              return "*";
-            }
-          },
+        title: Text(
+          "Bem vindo ao FORM",
+          style: TextStyle(color: Colors.black),
         ),
+        backgroundColor: Colors.greenAccent,
       ),
-      body: ListView.builder(
-        itemCount: widget.items.length,
-        itemBuilder: (BuildContext ctxt, int index) {
-          final item = widget.items[index];
-
-          return Dismissible(
-            child: CheckboxListTile(
-              title: Text(item.title ?? ""),
-              onChanged: (bool value) {
-                setState(() {
-                  item.done = value;
-                  save();
-                });
-              },
-              value: item.done,
-            ),
-            key: Key(item.title),
-            onDismissed: (direction) {
-              remove(index);
-            },
-            background: Container(
-              color: Theme.of(context).primaryColorLight,
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: add,
-        child: Icon(Icons.add),
-        backgroundColor: Colors.pink,
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[TextFormField()],
+          ),
+        ),
       ),
     );
   }
